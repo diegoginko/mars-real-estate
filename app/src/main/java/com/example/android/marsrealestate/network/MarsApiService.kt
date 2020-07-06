0b5ce1b736c9d0059366b3baa94eb03f6839da8b
Diego Ginko <diego.ginko@gmail.com>
2020-07-06 15:42:37 -0300

    Final

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,43 @@
 
 package com.example.android.marsrealestate.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+enum class MarsApiFilter(val value: String) { SHOW_RENT("rent"), SHOW_BUY("buy"), SHOW_ALL("all") }
+
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .baseUrl(BASE_URL)
+        .build()
+
+interface MarsApiService {
+    /**
+     * Returns a Coroutine [Deferred] [List] of [MarsProperty] which can be fetched with await() if
+     * in a Coroutine scope.
+     * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
+     * HTTP method
+     */
+    @GET("realestate")
+    fun getProperties(@Query("filter") type: String):
+    // The Coroutine Call Adapter allows us to return a Deferred, a Job with a result
+            Deferred<List<MarsProperty>>
+}
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
+}
